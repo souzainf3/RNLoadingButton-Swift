faabc33899ac4f83dd10623f04f3b5c4ccd7b6a1
Andrew Breckenridge <asbreckenridge@me.com>
2016-05-23 19:48:19 -0700

    create and expose RNLoadingButton target

---
+++ RNLoadingButton/RNLoadingButton.h
@@ -0,0 +1,19 @@
+//
+//  RNLoadingButton.h
+//  RNLoadingButton
+//
+//  Created by Andrew Breckenridge on 5/23/16.
+//  Copyright © 2016 Andrew Breckenridge. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RNLoadingButton.
+FOUNDATION_EXPORT double RNLoadingButtonVersionNumber;
+
+//! Project version string for RNLoadingButton.
+FOUNDATION_EXPORT const unsigned char RNLoadingButtonVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RNLoadingButton/PublicHeader.h>
+
+
