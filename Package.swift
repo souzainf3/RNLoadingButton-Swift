8416557c7e863bc28fdbc13351f8381b36109f0d
tommyhan <tommyhan@Tommy-Hans-Mac-mini.local>
2022-10-12 14:08:57 +0800

    Move required files to source folder

--- Package.swift
+++ Package.swift
@@ -21,7 +21,9 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "RNLoadingButton-Swift-SPM",
-            dependencies: []),
+            dependencies: [],
+            path: "Sources"
+        ),
         .testTarget(
             name: "RNLoadingButton-Swift-SPMTests",
             dependencies: ["RNLoadingButton-Swift-SPM"]),
--- Sources/RNLoadingButton-Swift-SPM/RNLoadingButton_Swift_SPM.swift
+++
@@ -1,6 +0,0 @@
-public struct RNLoadingButton_Swift_SPM {
-    public private(set) var text = "Hello, World!"
-
-    public init() {
-    }
-}
