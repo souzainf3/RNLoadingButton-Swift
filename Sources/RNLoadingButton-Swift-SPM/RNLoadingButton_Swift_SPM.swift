791ddf90c48186e30ca8c1835147b86738740634
tommyhan <tommyhan@Tommy-Hans-Mac-mini.local>
2022-10-12 12:47:07 +0800

    Add SPM support

---
+++ Sources/RNLoadingButton-Swift-SPM/RNLoadingButton_Swift_SPM.swift
@@ -0,0 +1,6 @@
+public struct RNLoadingButton_Swift_SPM {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
---
+++ Tests/RNLoadingButton-Swift-SPMTests/RNLoadingButton_Swift_SPMTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import RNLoadingButton_Swift_SPM
+
+final class RNLoadingButton_Swift_SPMTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(RNLoadingButton_Swift_SPM().text, "Hello, World!")
+    }
+}
